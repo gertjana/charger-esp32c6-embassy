ce4cabb591cdf1545d6b828773c03c29c85572f6
Gertjan Assies <gertjan.assies@gmail.com>
2025-09-20 14:11:30 +0200

    clippy suggestions

--- src/ntp.rs
+++ src/ntp.rs
@@ -491,5 +491,5 @@ fn days_to_date(mut days: u32) -> (u32, u32, u32) {
 }
 
 fn is_leap_year(year: u32) -> bool {
-    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+    (year.is_multiple_of(4) && !year.is_multiple_of(100)) || year.is_multiple_of(400)
 }
